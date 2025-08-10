fe3b586bf7c22c76ea99cdd81571491c133f4ad2
mitchelle lindsey <mitchellelindsey@gmail.com>
2025-08-10 23:42:29 +0100

    all tasks

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React from "react";
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <UserProfile user={{ avatar: "https://via.placeholder.com/150", name: "John Doe", bio: "Developer at Example Co. ..." }} />
+
+        
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+       <div className="bg-blue-500 text-white p-6 rounded-lg">
+      <h1 className="text-3xl font-bold">Hello Tailwind + React!</h1>
+    </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
